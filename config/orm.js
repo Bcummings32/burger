742f5d2eb5d5bdabdc7bb82b859cf0137a08d25f
Ben Cummings <bncmmngs@aol.com>
2020-12-23 13:14:53 -0500

    Recreate server

--- config/orm.js
+++ config/orm.js
@@ -1,90 +1,85 @@
 var connection = require("./connection.js");
-
-//function for sql syntax
 function printQuestionMarks(num) {
-    var arr = [];
-    for (var i = 0; i < num; i++) {
-        arr.push("?");
-    }
-    return arr.toString();
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
 }
-
-//function to convert to SQL syntax
 function objToSql(ob) {
-    var arr = [];
-    for (var key in ob) {
-        var value = ob[key];
-        if (Object.hasOwnProperty.call(ob, key)) {
-            if (typeof value === "string" && value.indexOf("") >= 0) {
-                value = "'" + value + "'";
-            }
-            arr.push(key + "=" + value);
-        }
-    }
-    return arr.toString();
+  // column1=value, column2=value2,...
+  var arr = [];
+  for (var key in ob) {
+    arr.push(key + "=" + ob[key]);
+  }
+  return arr.toString();
 }
-
-//display all burgers in the db
-//object oriented prog.
 var orm = {
-    selectAll: function(table, cb) {
-        var queryString = "SELECT * FROM" + table + ";";
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err;
-            }
-            cb(result);
-        })
-    },
- //add a burger to the db
-    Create: function(table, cols, vals, cb) {
-        var queryString = "INSERT INTO" + table;
-        queryString += " (";
-        queryString += cols.toString();
-        queryString += ") ";
-        queryString += "VALUES (";
-        queryString += printQuestionMarks(vals.length);
-        queryString += ") ";
+  all: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // vals is an array of values that we want to save to cols
+  // cols are the columns we want to insert the values into
+  create: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+    console.log(queryString);
+    connection.query(queryString, vals, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // objColVals would be the columns and values that you want to update
+  // an example of objColVals would be {name: panther, sleepy: true}
+  update: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  }
+};
+module.exports = orm;
 
-        connection.query(queryString, vals, function(err, result) {
-            if (err) {
-                throw err
-            }
-            cb(result);
-        });
-    },
 
-    // set devour status to true
-    Update: function(table, objColVals, condition, cb) {
-        var queryString = "UPDATE " + table;
-        queryString += " SET ";
-        queryString += objToSql(objColVals);
-        queryString += " WHERE ";
-        queryString += condition;
 
-        connection.query(queryString, function(err, result) {
-            if (err) {
-                throw err 
-            }
-            cb(result);
-        });
-    },
-        // delete burger
-        deleteOne: function (table, condition, cb) {
-            var queryString = "DELETE FROM " + table;
-            queryString += " WHERE ";
-            queryString += condition;
-    
-            connection.query(queryString, function(err, result) {
-                if (err) {
-                    throw err
-                }
-                cb(result);
-            });
-        }
 
-    }
 
-    
-   
-    module.exports = orm;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- db/seed.sql
+++
@@ -1,5 +0,0 @@
---write insert queries to pop. burger table with at least 3 entries
-INSERT INTO burgers (burger_name, devoured)     VALUES('Mushroom', 1);
-INSERT INTO burgers (burger_name, devoured)    VALUES('Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured)     VALUES('Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured)    VALUES('Turkey', false);
